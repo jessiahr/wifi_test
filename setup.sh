9f9ffc5ec10c4c11cf734bcc3084a5178c028e7c
Jessiah Ratliff <jessiahr@gmail.com>
2017-02-24 00:26:25 +0000

    Update and rename .gitignore to setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+sudo apt-get install ethtool rfkill libnl-3-dev libnl-genl-3-dev
+
+wget http://download.aircrack-ng.org/aircrack-ng-1.2-rc4.tar.gz
+ tar -zxvf aircrack-ng-1.2-rc4.tar.gz
+ cd aircrack-ng-1.2-rc4
+ make
+ make install
+
